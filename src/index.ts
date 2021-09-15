3cdbf0a99c2acfe6df70a8492f08b52fa02b4277
limitMe <z@limme.net>
2021-09-15 10:30:35 +0800

    Add typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import * as bodyParser from 'koa-bodyparser';
+import 'reflect-metadata';
 import { PORT } from './config';
 import AppRoutes from './routes';
 
