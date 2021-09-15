c26a81765c06b7df9a48078116a901d8283bbc14
limitMe <z@limme.net>
2021-09-15 11:57:59 +0800

    Lint

--- src/controller/home.ts
+++ src/controller/home.ts
@@ -6,31 +6,29 @@ import User from "../model/user";
  * Loads all users from the database.
  */
 export async function getAllAction(context: Context) {
+  const userRepository = getManager().getRepository(User);
 
-    const userRepository = getManager().getRepository(User);
+  const users = await userRepository.find();
 
-    const users = await userRepository.find();
-
-    context.body = users;
+  context.body = users;
 }
 
 /**
  * Create a user to the database.
  */
 export async function createAction(context: Context) {
+  const userRepository = getManager().getRepository(User);
 
-    const userRepository = getManager().getRepository(User);
+  const newUser = userRepository.create();
 
-    const newUser = userRepository.create()
+  newUser.name = '钟典';
+  newUser.constellation = '射手座';
+  newUser.pair = '李庆渝';
+  newUser.character1 = '乐善好施';
+  newUser.character2 = '帅气逼人';
+  newUser.character3 = '简直不摆了';
 
-    newUser.name = '钟典'
-    newUser.constellation = '射手座'
-    newUser.pair = '李庆渝'
-    newUser.character1 = '乐善好施'
-    newUser.character2 = '帅气逼人'
-    newUser.character3 = '简直不摆了'
+  await userRepository.save(newUser);
 
-    await userRepository.save(newUser)
-
-    context.body = newUser;
+  context.body = newUser;
 }
